6131b59e70349b4bd2633df66184e568400e4066
vaas99 <hicham@fabernovel.dm>
2017-11-20 19:20:32 +0100

    external json input

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tinyDSL
 Tiny DSL exercise
+
+- Both selection and filter functions are recursive .
+- We use parenthesis to keep the request logic -> [(a or b) and c] != [a or b and c]
+- JSON input defined in queryExample file .
--- main.py
+++ main.py
@@ -1,71 +1,42 @@
 import sys
-
-input_data = {
-'selector': ['a','b','c'],
-'filters': {'and':[
-{
-    'field': 'a',
-    'value': 200,
-    'predicate': 'gt'
-},
-{'or':[
-{
-    'field': 'b',
-    'value': 'ok',
-    'predicate': 'contains'
-},
-{
-    'field': 'c',
-    'value': 'none',
-    'predicate': 'contains'
-}]},
-{
-    'field': 'd',
-    'value': 3,
-    'predicate': 'gt'
-},
-{
-    'field': 'e',
-    'value': 3,
-    'predicate': 'lt'
-}
-]
-}}
+import json
 
 
-def main():
-    if 'selector' in input_data :
-        a = 'SELECT ' + selector(input_data['selector'])+' FROM crawls'
+def main() :
+    input_data = json.load(open('queryExample.json'))
+
+    if 'fields' in input_data :
+        a = 'SELECT ' + selector(input_data['fields']) + ' FROM crawls'
     else:
         print 'Selector field not defined'
         sys.exit(1)
 
     if 'filters' in input_data:
-        a+= ' WHERE '+filters(input_data['filters'])
+        a+= ' WHERE ' + filters(input_data['filters'])
     print (a)
 
 
 # recursive data selection function
 def selector (liste):
-    if len(liste)==0:
+    if len(liste) == 0:
         print 'no selector defined'
         sys.exit(1)
-    elif len(liste)==1:
+    elif len(liste) == 1:
         return liste[0]
     else:
-        return liste[0]+','+selector(liste[1:])
+        return liste[0] + ',' + selector(liste[1:])
 
-def filters(predicate):
+def filters(predicate) :
 
     if 'and' in predicate :
         if len(predicate['and']) > 1 :
-            return filters(predicate['and'][0]) +' AND '+ filters({'and':predicate['and'][1:]})
+            return '(' + filters(predicate['and'][0]) +' AND ' + filters({'and':predicate['and'][1:]}) + ')'
         else:
             return filters(predicate['and'][0])
 
     elif 'or' in predicate :
         if len(predicate['or']) > 1 :
-            return filters(predicate['or'][0]) +' OR '+ filters({'or':predicate['or'][1:]})
+            return '(' + filters(predicate['or'][0]) + ' OR ' + filters({'or':predicate['or'][1:]}) + ')'
         else:
             return filters(predicate['or'][0])
 
