7e3f584f534531e9082c272b515bd14f90109f5f
vaas99 <hicham@fabernovel.dm>
2017-11-20 02:18:59 +0100

    FILTERS field

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import sys
 
 input_data = {
-'selector': ['a','b','c']}
+'selector': ['a','b','c'],
+'filters': {'and':[
+{
+    'field': 'a',
+    'value': 200,
+    'predicate': 'gt'
+},
+{'or':[
+{
+    'field': 'b',
+    'value': 'ok',
+    'predicate': 'contains'
+},
+{
+    'field': 'c',
+    'value': 'none',
+    'predicate': 'contains'
+}]},
+{
+    'field': 'd',
+    'value': 3,
+    'predicate': 'gt'
+},
+{
+    'field': 'e',
+    'value': 3,
+    'predicate': 'lt'
+}
+]
+}}
 
 
 def main():
@@ -12,7 +41,7 @@ def main():
         sys.exit(1)
 
     if 'filters' in input_data:
-        pass
+        a+= ' WHERE '+filters(input_data['filters'])
     print (a)
 
 
@@ -27,7 +56,34 @@ def selector (liste):
         return liste[0]+','+selector(liste[1:])
 
 def filters(predicate):
-    pass
+
+    if 'and' in predicate :
+        if len(predicate['and']) > 1 :
+            return filters(predicate['and'][0]) +' AND '+ filters({'and':predicate['and'][1:]})
+        else:
+            return filters(predicate['and'][0])
+
+    elif 'or' in predicate :
+        if len(predicate['or']) > 1 :
+            return filters(predicate['or'][0]) +' OR '+ filters({'or':predicate['or'][1:]})
+        else:
+            return filters(predicate['or'][0])
+
+    else:
+        return predicate['field'] + ' ' + formatting(predicate['predicate'] , predicate['value'])
+
+def formatting(predicate,value):
+    if predicate == 'gt':
+        return '> ' + str(value)
+    elif predicate == 'equal':
+        return '= ' + str(value)
+    elif predicate == 'lt':
+        return '< ' + str(value)
+    elif predicate == 'contains':
+        return 'LIKE \'%' + str(value) +'%\''
+    else:
+        print 'unknown predicate'
+        sys.exit(0)
 
 
 if __name__ == '__main__':
